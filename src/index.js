9b692b8cefbe180a6ea0b1149ee400d617d23ae8
Faizan <kfaizan0496@gmail.com>
2024-11-28 16:18:21 +0530

    Add API Documentation on home page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express =require('express');
+const path=require('path')
 const dbConnect = require('./config/mongoose');
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+
+
 require('dotenv').config();
 
 const app=express();
 console.log(process.env.PORT);
-
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 // DataBase Connection
 
@@ -18,7 +22,10 @@ app.use(express.json())
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.get('/',(req,res)=>{
-    res.send('<h1>Welcome to Role Based Access Control Application</h1>')
+    // res.send('<h1>Welcome to Role Based Access Control Application</h1>')
+    res.render('index',{
+        title:"Role Based Access"
+    })
 })
 
 const Port=process.env.PORT || 3000;
